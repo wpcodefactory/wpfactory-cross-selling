403082c6f3eaba7db81cecd3118ff10418ee915b
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2024-09-24 21:50:38 -0300

    Progress

--- templates/product.php
+++ templates/product.php
@@ -1,16 +1,17 @@
 <?php
 $free_version_installed_class = $free_version_installed || $pro_version_installed ? 'disabled' : '';
 $pro_version_installed_class  = $pro_version_installed ? 'disabled' : '';
+$product_class  = $pro_version_installed ? 'readonly' : '';
 ?>
 
-<div class="wpfcs-product <?php echo esc_attr( $pro_version_installed_class ) ?>">
+<div class="wpfcs-product <?php echo esc_attr( $product_class ) ?>">
 	<?php if ( isset( $product_data['icon_url'] ) ): ?>
 		<div class="wpfcs-product-img-wrapper">
-			<img src="<?php echo esc_attr( $product_data['icon_url'] ) ?>"/>
+			<a href="<?php echo esc_url( $pro_plugin_url ) ?>" target="_blank"><img src="<?php echo esc_attr( $product_data['icon_url'] ) ?>"/></a>
 		</div>
 	<?php endif; ?>
 	<div class="wpfcs-product-middle">
-		<h3 class="wpfcs-product-title"><?php echo esc_html( $product_data['name'] ) ?></h3>
+		<h3 class="wpfcs-product-title"><a href="<?php echo esc_url( $pro_plugin_url ) ?>" target="_blank"><?php echo esc_html( $product_data['name'] ) ?></a></h3>
 		<p class="wpfcs-product-desc"><?php echo esc_html( $product_data['desc'] ) ?></p>
 	</div>
 	<div class="wpfcs-product-actions">
